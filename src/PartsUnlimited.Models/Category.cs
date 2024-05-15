ff089a8559af7c757139e50dfcd2b1dd885e3a06
Jhobany Peña Ortiz <jhpenaor@microsoft.com>
2024-05-15 11:56:52 -0500

    Cambio en Categoria

--- src/PartsUnlimited.Models/Category.cs
+++ src/PartsUnlimited.Models/Category.cs
@@ -6,6 +6,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PartsUnlimited.Models
 {
+    // My third change
     public class Category
     {
         public int CategoryId { get; set; }
